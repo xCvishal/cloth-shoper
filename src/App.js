f1c3845d33695aa252b07016abd56c81f2612c2e
xCvishal <vishalsingh2402@gmail.com>
2019-12-29 18:32:07 -0800

    finished styling menu-items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+
 import './App.css';
 
-import HomePage from './hompage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
